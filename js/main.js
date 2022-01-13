0509835c393e5e11b2abd6cfab319ca0aa469a42
EgidioSperanza <85298024+EgidioSperanza@users.noreply.github.com>
2022-01-13 14:14:16 +0100

    prima occhiata a Vue

---
+++ js/main.js
@@ -0,0 +1,11 @@
+new Vue ({
+    el: "#app",
+    data:{
+        userName:'',
+        titleMessage: 'Hello World',
+        userImage:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        userImageClass:'box-image'
+    },
+    methods:{
+    }
+})
